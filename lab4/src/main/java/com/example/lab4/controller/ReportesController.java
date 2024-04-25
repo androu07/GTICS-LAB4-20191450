7fd7de6221b8107280097b64eb84f6130a536f9c
andres <a20191450@pucp.edu.pe>
2024-04-24 20:33:19 -0500

    final

--- lab4/src/main/java/com/example/lab4/controller/ReportesController.java
+++ lab4/src/main/java/com/example/lab4/controller/ReportesController.java
@@ -3,8 +3,13 @@ package com.example.lab4.controller;
 import com.example.lab4.entity.Trabajo;
 import com.example.lab4.repository.TrabajoRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ReportesController {
@@ -22,9 +27,34 @@ public class ReportesController {
     }
 
     @GetMapping("/hr/reportes")
-    public String listarPreordenes(){
-
+    public String mostrarReportes(){
         return "reportes";
+    }
+
+    @GetMapping("/hr/reportes/salario")
+    public String mostrarSalarios(Model model){
+        List<Trabajo> listatrabajos = trabajoRepository.findAll();
+        model.addAttribute("listaTrabajos",listatrabajos);
+        return "salario";
+    }
+
+    @GetMapping("/editar")
+    public String nuevoTrabajo(Model model,
+                               @RequestParam("id") int id){
+        Optional<Trabajo> optionalTrabajo = trabajoRepository.findById(id);
+
+        if (optionalTrabajo.isPresent()){
+            Trabajo trabajo = optionalTrabajo.get();
+            model.addAttribute("trabajo", trabajo);
+            return "editartrabajo";
+        }
+
+        return "nuevotrabajo";
+    }
+
+    @GetMapping("/hr/trabajo/nuevo")
+    public String nuevoTrabajo(){
+        return "nuevotrabajo";
     }
 
     @PostMapping("/guardartrabajo")
